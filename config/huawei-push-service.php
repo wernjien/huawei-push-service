ef8d78f2d705e376bb8088f0db0fb6ca3582e260
Tan Wern Jien <wernjien@gmail.com>
2021-08-26 13:52:43 +0800

    Rework as static methods

--- config/huawei-push-service.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'client_id' => env('HUAWEI_CLIENT_ID'),
-    'grant_type' => env('HUAWEI_GRANT_TYPE'),
-    'client_secret' => env('HUAWEI_CLIENT_SECRET'),
-];
--- src/HuaweiPushService.php
+++ src/HuaweiPushService.php
@@ -7,101 +7,75 @@ use GuzzleHttp\Client;
 class HuaweiPushService
 {
     /**
-     * Get the config for the given key.
+     * Get the access token.
      *
-     * @param  string  $key
+     * @param  string  $clientId
+     * @param  string  $clientSecret
      * @return string
      */
-    public static function getConfig($key)
+    public static function getAccessToken($clientId, $clientSecret)
     {
-        if (function_exists('config')) {
-            return config('huawei-push-service.'.$key);
-        }
-
-        if (function_exists('getenv')) {
-            return getenv('HUAWEI_.'.strtoupper($key));
-        }
-
-        return '';
-    }
-
-    /**
-     * To get access token before sending notification.
-     *
-     * @return array
-     */
-    public static function getAccessToken()
-    {
-        $grantType = self::getConfig('grant_type');
-        $clientId = self::getConfig('client_id');
-        $clientSecret = self::getConfig('client_secret');
-
-        $client = new Client();
-        $response = $client->request('POST', 'https://oauth-login.cloud.huawei.com/oauth2/v3/token', [
+        $response = (new Client)->request('POST', 'https://oauth-login.cloud.huawei.com/oauth2/v3/token', [
             'headers' => [
                 'content-type' => 'application/x-www-form-urlencoded',
             ],
-            'body' => 'grant_type='.$grantType.'&client_id='.$clientId.'&client_secret='.$clientSecret.'',
+            'body' => "grant_type=client_credentials&client_id={$clientId}&client_secret={$clientSecret}",
         ]);
 
-        $result = $response->getBody()->getContents();
-
-        return json_decode($result, true);
+        $data = json_decode($response->getBody()->getContents());
+        $accessToken = isset($data->access_token) ? $data->access_token : '';
+        
+        return $accessToken;
     }
 
     /**
-     * Sent the notification.
+     * Send push notification.
      *
-     * @return array
+     * @param  string  $clientId
+     * @param  string  $accessToken
+     * @param  string  $title
+     * @param  string  $body
+     * @param  mixed  $tokens
+     * @param  string  $intent
+     * @return object
      */
-    public static function sendNotification($title, $body, $tokenDevice, $clickAction = '')
+    public static function sendNotification($clientId, $accessToken, $title, $body, $tokens, $intent = '')
     {
-        $generate = self::getAccessToken();
+        if (! is_array($tokens)) {
+            $tokens = [$tokens];
+        }
 
-        return self::sendMessageNotification($generate['access_token'], $title, $body, $tokenDevice, $clickAction);
-    }
-
-    /**
-     * notification parameters and sent to huawei API.
-     *
-     * @return array
-     */
-    public static function sendMessageNotification($token, $title, $body, $tokenDevice, $clickAction = '')
-    {
-        $param = [
+        $body = json_encode([
             'validate_only' => false,
             'message' => [
                 'notification' => [
                     'title' => $title,
-                    'body'  => $body,
+                    'body' => $body,
                 ],
                 'android' => [
                     'notification' => [
                         'title' => $title,
                         'body' => $body,
                         'click_action' => [
-                            'type'   => 1,
-                            'intent' => $clickAction,
+                            'type' => 1,
+                            'intent' => $intent,
                         ],
                     ],
                 ],
-                'token' => [$tokenDevice],
+                'token' => $tokens,
             ],
-        ];
-
-        $param = json_encode($param);
-
-        $request = new Client();
-        $response = $request->request('POST', 'https://push-api.cloud.huawei.com/v1/102714235/messages:send', [
-            'headers' => [
-                'Authorization' => 'Bearer '.$token,
-                'Accept' => 'application/json',
-                'content-type'  => 'application/json; charset=utf8',
-            ],
-            'connect_timeout' => 30,
-            'body' => $param,
         ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        $response = (new Client)->request('POST', "https://push-api.cloud.huawei.com/v1/{$clientId}/messages:send", [
+            'headers' => [
+                'Authorization' => "Bearer {$accessToken}",
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json; charset=utf8',
+            ],
+            'connect_timeout' => 30,
+            'body' => $body,
+        ]);
+
+        return json_decode($response->getBody()->getContents());
     }
 }
